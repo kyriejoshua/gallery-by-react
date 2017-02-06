ed9f969ae782933ded2e42715f4ed4b82b6bb28f
zhengyuan zhu <zhuzy@broada.com>
2017-02-06 15:03:44 +0800

    feat: 大致布局完成 chore: 添加json依赖包 style: 大致布局样式完成        新增部分图片

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,38 @@
 require('normalize.css/normalize.css');
 require('styles/App.scss');
+let imageDatas = require('../data/images.json');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+
+/**
+ * [getImagesUrl 获取图片URL路径]
+ * @param  {Array} imageDatasArr [description]
+ * @return {Array}               [description]
+ */
+imageDatas = (function getImagesUrl(imageDataArr) {
+  for (let i = 0; i < imageDataArr.length; i++) {
+    let singleImageData = imageDataArr[i];
+    singleImageData.imageURL = require(`../images/${singleImageData.fileName}`);
+    imageDataArr[i] = singleImageData.fileName;
+  }
+  return imageDataArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="section-img">
+        </section>
+        <nav className="nav-controller">
+        </nav>
+      </section>
+      // <div className="index">
+      //   <img src={yeomanImage} alt="Yeoman Generator" />
+      //   <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      // </div>
     );
   }
 }
